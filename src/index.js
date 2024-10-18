1f0e57a18b5baedbcc38f8fa65663fc0b0829c22
devsoftwaree <devsoftware.official@gmail.com>
2024-10-18 12:01:46 +0700

    third commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-// Express library
+const path = require('path')
 const express = require('express')
+const morgan = require('morgan')
+const { engine: handlebars } = require('express-handlebars')
+
+// Express library
 const app = express()
 const port = 3000
 
+// Static file
+app.use(express.static(path.join(__dirname,'public')))
+
 // HTTP loger
-const morgan = require('morgan')
 app.use(morgan('combined'))
 
 // Template engine
-const handlebars = require('express-handlebars')
-app.engine('handlebars', handlebars())
-app.set('view engine', 'handlebars')
+app.engine('hbs', handlebars({
+	extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resource/views'))
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
 })
 
 app.listen(port, () => {
